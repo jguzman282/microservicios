96d56900fdb5fecbdd0a287f16567fa00a48cf7e
Joaquin Guzman <joaquin.guzman@ucse.net.ar>
2022-09-20 15:48:48 -0300

    con autenticacion

--- apps/microservicios/src/app.service.ts
+++ apps/microservicios/src/app.service.ts
@@ -1,5 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+var ActiveDirectory = require('activedirectory');
+
+var ActiveDirectory = require('activedirectory');
+var config = {
+  url: 'ldap://ucse.net.ar',
+  baseDN: 'dc=ucse.net.ar,dc=com',
+};
+var ad = new ActiveDirectory(config);
+var username = 'joaquin.guzman@ucse.net.ar';
+var password = 'Ucse2022';
 
 @Injectable()
 export class AppService {
@@ -11,6 +21,20 @@ export class AppService {
 
   newLogin(user: any) {
     this.clientLogin.emit('new_login', user);
+
+    // Authenticate
+    ad.authenticate(username, password, function (err, auth) {
+      if (err) {
+        console.log('ERROR: ' + JSON.stringify(err));
+        console.log('NO SE CONECTO NADAAAAAAAA');
+        return 'no se conecto';
+      }
+      if (auth) {
+        console.log('Authenticated!', auth);
+      } else {
+        console.log('Authentication failed!');
+      }
+    });
     return 'send_queue';
   }
 }
